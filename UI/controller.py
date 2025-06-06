b7c358e107c78f546203b3e42ff206f1bdc4f46e
Ekaterina-Velikozhon <velikozhon.e@gmail.com>
2025-06-06 19:33:12 +0200

    Update controller.py

--- UI/controller.py
+++ UI/controller.py
@@ -20,9 +20,92 @@ class Controller:
         self._view.update_page()
 
     def handleCreaGrafo(self, e):
-        pass
+        store = self._view._ddStore.value
+        if not store:
+            self._view.txt_result.controls.clear()
+            self._view.txt_result.controls.append(ft.Text("Seleziona un store", color="red"))
+            self._view.update_page()
+            return
+
+        k = self._view._txtIntK.value
+        if not k:
+            self._view.txt_result.controls.clear()
+            self._view.txt_result.controls.append(ft.Text("Scegli un numero di giorni", color="red"))
+            self._view.update_page()
+            return
+
+        try:
+            kInt = int(k)
+        except ValueError:
+            print("Formato sbagliato")
+            return
+
+
+        self._model.buildGraph(store, kInt)
+        self.fillDDNodi()
+
+        nodes, edges = self._model.getGraphDetails()
+
+        self._view.txt_result.controls.clear()
+        self._view.txt_result.controls.append(ft.Text("Grafo correttamente creato:"))
+        self._view.txt_result.controls.append(ft.Text(f"Numero di nodi: {nodes}"))
+        self._view.txt_result.controls.append(ft.Text(f"Numero di archi: {edges}"))
+
+        self._view.update_page()
+
+
+    def fillDDNodi(self):
+        store = self._view._ddStore.value
+        if not store:
+            self._view.txt_result.controls.clear()
+            self._view._txt_result.controls.append(ft.Text("Seleziona un store", color="red"))
+            self._view.update_page()
+            return
+
+        self._view._ddNode.options.clear()
+        ordini = self._model.getNodi(store)
+
+        for o in ordini:
+            self._view._ddNode.options.append(ft.dropdown.Option(text=o.order_id,
+                                                                  data=o))
+        self._view.update_page()
+
     def handleCerca(self, e):
-        pass
+        nodoPartenza = self._view._ddNode.value
+        if not nodoPartenza:
+            self._view.txt_result.controls.clear()
+            self._view.txt_result.controls.append(ft.Text("Scegli un nodo da cui vuoi partirei", color="red"))
+            self._view.update_page()
+            return
+
+        cammino = self._model.camminoPiuLungo(int(nodoPartenza)) #meglio fare con try+except
+
+        self._view.txt_result.controls.append(ft.Text(f"Nodo di partenza: {nodoPartenza}"))
+        for n in cammino[1:]:
+            self._view.txt_result.controls.append(ft.Text(n))
+
+        self._view.update_page()
 
     def handleRicorsione(self, e):
-        pass
+        nodoPartenza = self._view._ddNode.value
+        if not nodoPartenza:
+            self._view.txt_result.controls.clear()
+            self._view.txt_result.controls.append(ft.Text("Scegli un nodo da cui vuoi partirei", color="red"))
+            self._view.update_page()
+            return
+
+        try:
+            nodoId = int(nodoPartenza)
+        except ValueError:
+            print("Il formato sbagliato")
+            return
+
+        bestpath, bestscore = self._model.getBestPath(nodoId)
+        self._view.txt_result.controls.clear()
+        self._view.txt_result.controls.append(
+            ft.Text(f"Trovato un cammino che parte da {nodoId} "
+                    f"con somma dei pesi uguale a {bestscore}."))
+
+        for v in bestpath:
+            self._view.txt_result.controls.append(ft.Text(f"{v[0]} -> {v[1]}: {v[2]}"))
+        self._view.update_page()
